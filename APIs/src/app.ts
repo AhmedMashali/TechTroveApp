39242cd0aa176c30df6b83f3c55f318f3d97861d
Ahmed-Mashali <ahmed.mashali@vultara.com>
2025-07-09 05:13:07 +0300

    Add error middlewares

--- APIs/src/app.ts
+++ APIs/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application } from 'express';
 
 import corsOptions from './config/cors.config';
+import { notFoundHandler } from './middlewares/notFound.middleware';
+import { errorHandler } from './middlewares/error.middleware';
 
 const app: Application = express();
 
@@ -9,8 +11,12 @@ app.use(corsOptions);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+app.get('/', (req, res, next) => {
     res.status(200).json({ msg: 'welcome to the app!' });
 });
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 export default app;
