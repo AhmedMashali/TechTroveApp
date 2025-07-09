44d4f0d02b63f7983142b347b5674edec6fc98e0
Ahmed-Mashali <ahmed.mashali@vultara.com>
2025-07-09 04:50:27 +0300

    Add database configuration

---
+++ APIs/src/config/db.config.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { env } from './env.config';
+
+const connectDB = async (): Promise<void> => {
+    try {
+        if (!env.MONGODB_URI) {
+            throw new Error('MONGODB_URI is not defined');
+        }
+
+        await mongoose.connect(env.MONGODB_URI);
+        console.log('Database is connected successfully');
+    } catch (error) {
+        console.error(`Database connection error: ${(error as Error).message}`);
+        process.exit(1);
+    }
+};
+
+mongoose.connection.on('error', err => {
+    console.error(`MongoDB connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+export default connectDB;
