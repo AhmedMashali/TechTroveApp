b17b9ca6129d7e654f5cf6e9fe5eaf15dac39fca
Ahmed-Mashali <ahmed.mashali@vultara.com>
2025-07-15 04:07:54 +0300

    Update axios instance

--- Frontend/src/lib/axios.ts
+++ Frontend/src/lib/axios.ts
@@ -1,8 +1,9 @@
-import { getAccessToken, setAccessToken } from '@/store/auth';
+import { BASE_URL } from '@/constants/APIs';
+import { getAccessToken } from '@/store/auth';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: BASE_URL,
     withCredentials: true,
 });
 
@@ -13,29 +14,5 @@ axiosInstance.interceptors.request.use((config) => {
     }
     return config;
 });
-
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
-
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-            try {
-                const { data } = await axiosInstance.post(
-                    '/auth/refresh-token'
-                );
-                setAccessToken(data.accessToken);
-                originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
-                return axiosInstance(originalRequest);
-            } catch (refreshError) {
-                console.error('Refresh token invalid');
-                return Promise.reject(refreshError);
-            }
-        }
-
-        return Promise.reject(error);
-    }
-);
 
 export default axiosInstance;
